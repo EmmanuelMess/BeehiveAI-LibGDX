60f97592b25baf7f330c19264ea0fbd33847a890
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-10-24 18:50:54 -0300

    Add project

---
+++ desktop/src/com/emmanuelmess/beehiveai/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.emmanuelmess.beehiveai.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.emmanuelmess.beehiveai.BeehiveAI
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(BeehiveAI(), config)
+    }
+}
