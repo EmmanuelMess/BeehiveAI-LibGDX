906ac29a2721b168bce224db7b52574e46fd4316
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-10-28 19:14:39 -0300

    Snap blocks to grid

--- core/src/com/emmanuelmess/beehiveai/actions/CreateBlockAtAction.kt
+++ core/src/com/emmanuelmess/beehiveai/actions/CreateBlockAtAction.kt
@@ -1,13 +1,14 @@
 package com.emmanuelmess.beehiveai.actions
 
 import com.badlogic.gdx.scenes.scene2d.Action
+import com.badlogic.gdx.utils.Align.topLeft
 import com.emmanuelmess.beehiveai.Game
 import com.emmanuelmess.beehiveai.actors.BlockActor
 
 class CreateBlockAtAction(val x: Int, val y: Int): Action() {
     override fun act(delta: Float): Boolean {
         Game.blockGroup.addActor(BlockActor().also {
-            it.setPosition(x.toFloat(), y.toFloat())
+            it.setPosition(x.toFloat(), y.toFloat(), topLeft)
         })
         return true
     }
